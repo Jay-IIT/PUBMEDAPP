4b2811b375636aae060656b7441bfedb867ca42a
Jayavelu Balaji <jbalaji@hawk.iit.edu>
2024-04-10 20:31:20 -0400

    Added Multiprocessing Support

--- ingest.py
+++ ingest.py
@@ -4,48 +4,53 @@ from core import pubmed_search
 from core import pubmed_batch_download
 import uuid
 import multiprocessing as mp
-from snowflake.sqlalchemy import URL
-from sqlalchemy import create_engine
-from snowflake.connector.pandas_tools import pd_writer
-import pandas as pd 
+from snowflake.connector.connect import Connect
+ 
 
 
 # Set up logging
 logging.basicConfig(filename='ingest.log', level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
 
+conn_params = {
+    "user": "TEST_USR_ETL",
+    "password": "8H_3TL@73sT",
+    "account": "dn13102.us-east-1",
+    "warehouse": "ETL_TEST",
+    "database": "BPDWD",
+    "schema": "AD_HOC"
+}
+
+
 pubmed_log_id = str(uuid.uuid4())
 logging.info(f"pubmed_log_id: {pubmed_log_id}")
 connection = None
-# Establish connection parameters
+ 
 
-def create_engine():
-    engine = create_engine(URL(user='TEST_USR_ETL',
-                               password='8H_3TL@73sT',
-                               account='dn13102.us-east-1',
-                               warehouse='ETL_TEST',
-                               database='BPDWD',
-                               schema='AD_HOC'
-                               ))
-    return engine
+def get_cursor():
+    connection = Connect(**conn_params)
+    cursor = connection.cursor()
+
+    return connection,cursor
 
 def update_log_table(processed, error_log=None, status="FINISHED"):
     try:
         global pubmed_log_id
-        engine = create_engine()
+        cursor,connection = get_cursor()
         if error_log:
-            engine.execute("""
+            cursor.execute("""
                 UPDATE PUBMED_DATA_LOG 
                 SET PROCESSED = %s, ERROR_LOG = %s,
                     STATUS = %s, END_TIME = CURRENT_TIMESTAMP()
                 WHERE PUBMED_LOG_ID = %s
             """, (processed, error_log, status,pubmed_log_id))
         else:
-            engine.execute("""
+            cursor.execute("""
                 UPDATE PUBMED_DATA_LOG 
                 SET PROCESSED = %s, END_TIME = CURRENT_TIMESTAMP(),STATUS = %s
                 WHERE PUBMED_LOG_ID = %s
             """, (processed,status,pubmed_log_id))
-        engine.dispose()
+        cursor.close()
+        connection.commit()
     except Exception as e:
         logging.error("Error updating Log Table into Snowflake: %s, for logid %s", e, pubmed_log_id)
         exit(0)
@@ -53,28 +58,63 @@ def update_log_table(processed, error_log=None, status="FINISHED"):
 # Function to insert a row into the table  
 def insert_to_log_table(row):
     try:
-        engine = create_engine()
-        df = pd.DataFrame(row)
-        df.to_sql("BPDWD.AD_HOC.PUBMED_DATA_LOG", engine, if_exists='replace', index=False,method=pd_writer)
-        engine.dispose()
+        cursor,connection = get_cursor()
+        columns = ', '.join(row.keys())
+        values = ', '.join('%s' for _ in row)
+        sql = f"""
+            INSERT INTO BPDWD.AD_HOC.PUBMED_DATA_LOG 
+                ({columns})
+            VALUES 
+                ({values})
+        """
+
+        # Execute the SQL query with values
+        cursor.execute(sql, tuple(row.values()))
+        cursor.close()
+        connection.commit() 
     except Exception as e:
         logging.error("Error while inserting log Table into Snowflake: %s, Search key word %s", e, row["SEARCH_KEYWORD"])
         exit(0)
 
 # Function to insert data into the table
-def insert_to_datatbl(data_rows):
+def insert_to_datatbl(data_rows, cursor):
     try:
-        engine = create_engine()
-        df = pd.DataFrame(data_rows)
-        df.to_sql("PUBMED_DATA", engine, if_exists='replace', index=False,method=pd_writer)
-        engine.dispose()  
+        # Extracting data from data_rows and preparing it for bulk insert
+        values = [(row['PMID'], row['SEARCH_TERM'], row['TITLE'], row['ABSTRACT'],
+                   row['AUTHOR_LIST'], row['KEYWORD_LIST'], pubmed_log_id) for row in data_rows]
+
+        # Performing bulk insert
+        cursor.executemany("""
+            INSERT INTO PUBMED_DATA (PMID, SEARCH_TERM, TITLE, ABSTRACT, AUTHOR_LIST, KEYWORD_LIST, PUBMED_LOG_ID)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)""", values)
     except Exception as e:
-        logging.error("Error inserting data into Snowflake: %s", e)
         raise e
 
-def fetch_and_upload(search_term, pubmedids_list):
-    processed = 0
+def process_batch(args):
+    start, end, search_term, search_results, batch_size = args
     try:
+        result = pubmed_batch_download(search_term, search_results, batch_size, start)
+        insert_to_datatbl(result)
+        logging.info(f"Downloaded Data {start} to {end}")
+        return end - start
+    except Exception as e:
+        logging.error("Error inserting data into Snowflake: {e} for Records {start} to {end}")
+        raise e
+         
+
+def fetch_and_upload(search_results, search_term):
+    try:
+        batch_size = 500
+        num_processes = min(10,mp.cpu_count()) # Number of CPU cores
+
+        # Create a process pool
+        with mp.Pool(processes=num_processes) as pool:
+            args_list = [(start, min(count, start + batch_size), search_term, search_results, batch_size) 
+                            for start in range(0, count, batch_size)]
+            processed_counts = pool.map(process_batch, args_list)
+
+        # After all processes are finished, update log table
+        processed = sum(processed_counts)
         update_log_table(processed)
     except Exception as e:
         logging.error("An error occurred in the fetch_and_upload: %s", e)
@@ -83,7 +123,7 @@ def fetch_and_upload(search_term, pubmedids_list):
 if __name__ == "__main__":
     try:
         logging.info(f"Ingestion Application Started")
-        if len(sys.argv) != 2:
+        if len(sys.argv) <= 2:
             logging.error("Usage: python ingest.py <search_term>")
             sys.exit(1)
         search_term = sys.argv[1]
@@ -98,11 +138,11 @@ if __name__ == "__main__":
                            "SEARCH_KEYWORD":search_term, 
                             "TOTAL_PMIDS":count, 
                             "STATUS":"STARTED",
-                            "FROM_DATE":str(min_date),
-                            "TO_DATE":str(min_date),
+                            "FROM_DATE":str(min_date) if min_date is not None else "N/A",
+                            "TO_DATE":str(max_date) if max_date is not None else "N/A",
                             "START_TIME":"CURRENT_TIMESTAMP()"}
         insert_to_log_table(log_table_entry)
-        update_log_table(0)
+        fetch_and_upload(search_results,search_term)
         logging.info(f"Ingestion Application Stopped Successfully")
     except Exception as e:
         logging.error("An error occurred in the main process: %s", e)
